355982eb66d6dc3f5eec1d157bc0c2c1597c61a2
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-10-20 07:54:23 -0500

    new push

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+      <nav>
+
+      </nav>
+    </header>
   )
 }
 
