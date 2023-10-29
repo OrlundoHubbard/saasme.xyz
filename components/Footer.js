7a1d14d8172158e8d42c8e4f96a14102b3141480
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-10-28 20:53:37 -0500

    new orlundo newwwwww

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <div className="border border-red-500">
-           <p>SaaS<span className="text-indigo-700">Me</span></p> 
+        <div className="items-center justify-center flex flex-col mt-16">
+           <p className="text-2xl font-bold">SaaS<span className="text-indigo-700">Me</span></p> 
+           <p className="mt-3">Contact Us</p>
         </div>
     </footer>
   )
